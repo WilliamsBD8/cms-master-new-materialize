6509f460b3b8c690ab59c192ffa1a9e263c5697f
WilliamsBD8 <wsbonilladiaz@gmail.com>
2025-10-28 21:04:48 -0500

    Add AuditController for managing database triggers and routes for audit functionality. Implement methods to create, drop, and export triggers, ensuring operations are restricted to development environment. Update Routes.php to include new audit route.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'HomeController::home');
+$routes->get('/audit', 'AuditController::createTriggers');
 
 $routes->get('/chat', 'PruebaController::chat');
 $routes->get('/datatable/(:segment)', 'PruebaController::datatable/$1');
